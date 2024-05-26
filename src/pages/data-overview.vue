a3bdb85347710adbc60cb0b8b6528884ec0c035c
creator-G <68057393+creator-G@users.noreply.github.com>
2024-05-27 00:00:33 +0200

    new data overview

---
+++ src/pages/data-overview.vue
@@ -0,0 +1,173 @@
+<template>
+  <v-app>
+    <!--structure define-->
+    <v-main>
+      <!--main content-->
+      <v-container fluid>
+        <v-row>
+          <v-col cols="12">
+            <v-card>
+              <!--Tab bar name-->
+              <v-toolbar color="indigo-darken-1">
+                <v-toolbar-title>Data Overview</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <template v-slot:extension>
+                  <v-tabs v-model="model" align-tabs="center">
+                    <v-tab
+                      v-for="tab in tabs"
+                      :key="tab.value"
+                      :text="tab.name"
+                      :value="tab.value"
+                    ></v-tab>
+                  </v-tabs>
+                </template>
+              </v-toolbar>
+
+              <!--Tab content-->
+              <v-tabs-window v-model="model">
+                <v-tabs-window-item
+                  v-for="tab in tabs"
+                  :key="tab.value"
+                  :value="tab.value"
+                >
+                  <v-card>
+                    <v-card-text>
+                      <!-- Content for first tab -->
+                      <template v-if="tab.value === 1">
+                        <!-- Figures -->
+                        <v-row>
+                          <!--link figure-->
+                          <v-col cols="6">
+                            <figure>
+                              <img
+                                src="https://blogs.sas.com/content/graphicallyspeaking/files/2019/02/nc_population_age_gender.png"
+                                alt="Population"
+                                width="500"
+                                height="500"
+                              />
+                            </figure>
+                          </v-col>
+                          
+                          <!--recht figure-->
+                          <v-col cols="6">
+                            <figure>
+                              <img
+                                src="https://static.packt-cdn.com/products/9781788295260/graphics/5a534f47-8593-4fd1-bbbe-e3c801a7b8ad.png"
+                                alt="Population"
+                                width="500"
+                                height="300"
+                              />
+                            </figure>
+                          </v-col>
+                        </v-row>
+
+                        <!--Table with search-->
+                        <r-row>
+                          <v-col cols="12">
+                            <!--search field-->
+                            <v-text-field
+                              v-model="search"
+                              label="Search"
+                              prepend-inner-icon="mdi-magnify"
+                              variant="outlined"
+                              hide-details
+                              single-line
+                            ></v-text-field>
+                            <!--table content-->
+                            <v-data-table
+                              :headers="columns"
+                              :items="rows"
+                              :search="search"
+                            ></v-data-table>
+                          </v-col>
+                        </r-row>
+                      </template>
+
+                      <!-- Content for second tab -->
+                      <template v-else-if="tab.value === 2">
+                        <!--Figure-->
+                        <figure>
+                          <img src="" alt="Figure related to Extension1" />
+                        </figure>
+                        <table>
+                          <!-- Table -->
+                        </table>
+                      </template>
+
+                      <!-- Content for the last tab -->
+                      <template v-else>
+                        <!--Figure-->
+                        <figure>
+                          <img src="" alt="Figure related to the Extension 2" />
+                        </figure>
+                        <table>
+                          <!-- Table -->
+                        </table>
+                      </template>
+                    </v-card-text>
+                  </v-card>
+                </v-tabs-window-item>
+              </v-tabs-window>
+            </v-card>
+          </v-col>
+
+          <!--Data Tables-->
+          <v-col cols="12"> </v-col>
+
+          <!--End of the content-->
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  // don't forget to export the about us file
+  name: "DataOverview",
+  data() {
+    return {
+      model: "tab-2",
+      //tab names
+      tabs: [
+        { name: "Population", value: 1 },
+        { name: "Extension1", value: 2 },
+        { name: "Extension2", value: 3 },
+      ],
+      //data for data table
+      search: "",
+      columns: [
+        {
+          align: "start",
+          key: "name",
+          sortable: false,
+          title: "Name",
+        },
+        { key: "column1", title: "Column1" },
+        { key: "column2", title: "Column2" },
+        { key: "column3", title: "Column3" },
+      ],
+      rows: [
+        {
+          name: "Row1",
+          column1: 1,
+          column2: 1,
+          column3: 1,
+        },
+        {
+          name: "Row2",
+          column1: 1,
+          column2: 1,
+          column3: 1,
+        },
+        {
+          name: "Row3",
+          column1: 1,
+          column2: 1,
+          column3: 1,
+        },
+      ],
+    };
+  },
+};
+</script>
