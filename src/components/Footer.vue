0bd99f25107c58b69fddf2756c9d0f96525b64dc
creator-G <68057393+creator-G@users.noreply.github.com>
2024-08-21 15:17:56 +0200

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,50 +1,12 @@
 <template>
-  <v-footer class="page-borders-footer" color="#FAFAFA" app>
+  <v-footer height="50" class="page-borders-footer" color="#FAFAFA" app>
     <v-row justify="center" no-gutters>
       <v-col cols="12" class="text-center">
-        <v-btn
-          v-for="link in links"
-          :key="link"
-          class="mx-2"
-          color="#757575"
-          variant="text"
-          :href="getLinkUrl(link)"
-        >
-          <strong>{{ link }}</strong>
-        </v-btn>
-      </v-col>
-      <v-col cols="12" class="text-center">
-        <p class="white--text">@{{ new Date().getFullYear() }} — DyHealthNet</p>
+        <p class="white--text">©{{ new Date().getFullYear() }} — DyHealthNet</p>
       </v-col>
     </v-row>
   </v-footer>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      links: ["Home", "Data Overview", "Network", "API", "Contact"],
-    };
-  },
-  methods: {
-    getLinkUrl(link) {
-      // Define the URL for each link
-      if (link === "Home") {
-        return "/";
-      } else if (link === "Data Overview") {
-        return "/overview";
-      } else if (link === "Network") {
-        return "/";
-      } else if (link === "API") {
-        return "/";
-      } else if (link === "Contact") {
-        return "/about";
-      }
-    },
-  },
-};
-</script>
 
 <style>
 .page-borders-footer {
