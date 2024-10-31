70f1459928bcc2ac0a0d26c25b7f1f6a1defa274
Eeeeelias <Elias.Albrecht@campus.lmu.de>
2024-10-31 16:13:34 +0100

    updated navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,29 +1,23 @@
 <template>
   <v-app-bar class="page-borders-nav" height="110" color="#FAFAFA" scroll-behavior="elevate" app>
-    <v-image>
       <img src="../assets/figures/DyHealthNet_Logo.png" width="270" height="140"/>
-    </v-image>
     <v-app-bar-title class="text-indigo"
       ></v-app-bar-title
     >
     <v-spacer></v-spacer>
     <v-menu transition="slide-x-transition">
       <template v-slot:activator="{ props }">
-        <a href="/">
-          <v-btn color="#104D63" v-bind="props">Home</v-btn>
-        </a>
+          <v-btn to="/" color="#104D63" class="mx-1" v-bind="props">Home</v-btn>
       </template>
     </v-menu>
     <v-menu transition="slide-x-transition">
       <template v-slot:activator="{ props }">
-        <a href="/overview" >
-          <v-btn color="#104D63" v-bind="props">Data Overview</v-btn>
-        </a>
+          <v-btn to="/overview" color="#104D63" class="mx-1" v-bind="props">Data Overview</v-btn>
       </template>
     </v-menu>
     <v-menu transition="slide-x-transition">
       <template v-slot:activator="{ props }">
-        <v-btn color="#104D63" v-bind="props">Network</v-btn>
+          <v-btn to="/network" color="#104D63" class="mx-1" v-bind="props">Network</v-btn>
       </template>
     </v-menu>
     <!--<v-menu transition="slide-x-transition">
@@ -33,9 +27,7 @@
     </v-menu>-->
     <v-menu transition="slide-x-transition">
       <template v-slot:activator="{ props }">
-        <a href="/about">
-          <v-btn color="#104D63" v-bind="props"> About Us</v-btn>
-        </a>
+          <v-btn to="/about" color="#104D63" class="mx-1" v-bind="props"> About Us</v-btn>
       </template>
     </v-menu>
   </v-app-bar>
