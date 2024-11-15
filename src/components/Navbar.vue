0cba29a3d292e66664337e067eceeaa55373ff8c
Eeeeelias <Elias.Albrecht@campus.lmu.de>
2024-11-15 10:11:13 +0100

    add icons to buttons

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -17,22 +17,38 @@
 
     <v-menu transition="slide-x-transition">
       <template v-slot:activator="{ props }">
-          <v-btn to="/" color="primary-darken-1" cl="mx-1" v-bind="props">Home</v-btn>
+          <v-btn to="/" color="primary-darken-1" cl="mx-1" v-bind="props">
+            <template v-slot:prepend>
+              <v-icon>mdi-home</v-icon>
+            </template>
+            Home</v-btn>
       </template>
     </v-menu>
     <v-menu transition="slide-x-transition">
       <template v-slot:activator="{ props }">
-        <v-btn to="/context" color="primary-darken-1" class="mx-1" v-bind="props">Context Creation</v-btn>
+        <v-btn to="/context" color="primary-darken-1" class="mx-1" v-bind="props">
+          <template v-slot:prepend>
+            <v-icon>mdi-tune-vertical</v-icon>
+          </template>
+          Context Creation</v-btn>
       </template>
     </v-menu>
     <v-menu transition="slide-x-transition">
       <template v-slot:activator="{ props }">
-          <v-btn to="/overview" color="primary-darken-1" class="mx-1" v-bind="props">Data Overview</v-btn>
+          <v-btn to="/overview" color="primary-darken-1" class="mx-1" v-bind="props">
+            <template v-slot:prepend>
+              <v-icon>mdi-chart-line</v-icon>
+            </template>
+            Data Overview</v-btn>
       </template>
     </v-menu>
     <v-menu transition="slide-x-transition">
       <template v-slot:activator="{ props }">
-          <v-btn to="/network" color="primary-darken-1" class="mx-1" v-bind="props">Network</v-btn>
+          <v-btn to="/network" color="primary-darken-1" class="mx-1" v-bind="props">
+            <template v-slot:prepend>
+              <v-icon>mdi-graph-outline</v-icon>
+            </template>
+            Network</v-btn>
       </template>
     </v-menu>
     <!--<v-menu transition="slide-x-transition">
@@ -42,7 +58,11 @@
     </v-menu>-->
     <v-menu transition="slide-x-transition">
       <template v-slot:activator="{ props }">
-          <v-btn to="/about-us" color="primary-darken-1" class="mx-1" v-bind="props"> About Us</v-btn>
+          <v-btn to="/about-us" color="primary-darken-1" class="mx-1" v-bind="props">
+            <template v-slot:prepend>
+              <v-icon>mdi-account-group-outline</v-icon>
+            </template>
+            About Us</v-btn>
       </template>
     </v-menu>
     <v-menu>
