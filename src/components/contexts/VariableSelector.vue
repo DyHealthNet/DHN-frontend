0375b6687e0cfbb9ac9ecf85bf1e3195bb359cfa
bastiennerehor <bastienne.rehor@tum.de>
2026-08-24 16:53:32 +0200

    final beauty tweak

--- src/components/contexts/VariableSelector.vue
+++ src/components/contexts/VariableSelector.vue
@@ -1,33 +1,38 @@
 <template>
-  <v-autocomplete
-      v-model="internalValue"
-      v-model:search="searchQuery"
-      :items="filteredItems"
-      :readonly="disableSelections"
-      multiple
-      density="compact"
-      variant="outlined"
-      :placeholder="items.length ? 'Select variables...' : 'No variables available'"
-  >
-    <template v-slot:selection="{ index }">
-      <span v-if="index === 0" class="text-body-2">
-        {{ internalValue.length }} variable{{ internalValue.length === 1 ? '' : 's' }} selected
-      </span>
-    </template>
-    <template v-slot:prepend-item>
-      <v-list-item title="Select All" :disabled="disableSelections" @click="selectAll">
-        <template v-slot:prepend>
-          <v-icon>mdi-checkbox-multiple-marked-outline</v-icon>
-        </template>
-      </v-list-item>
-      <v-list-item title="Deselect All" :disabled="disableSelections" @click="deselectAll">
-        <template v-slot:prepend>
-          <v-icon>mdi-checkbox-multiple-blank-outline</v-icon>
-        </template>
-      </v-list-item>
-      <v-divider class="mt-2"></v-divider>
-    </template>
-  </v-autocomplete>
+  <div>
+    <v-autocomplete
+        v-model="internalValue"
+        v-model:search="searchQuery"
+        :items="filteredItems"
+        :readonly="disableSelections"
+        multiple
+        density="compact"
+        variant="outlined"
+        :placeholder="items.length ? 'Select variables...' : 'No variables available'"
+    >
+      <template v-slot:selection="{ index }">
+        <span v-if="index === 0" class="text-body-2">
+          {{ internalValue.length }} variable{{ internalValue.length === 1 ? '' : 's' }} selected
+        </span>
+      </template>
+    </v-autocomplete>
+    <div class="d-flex justify-end">
+      <v-btn
+          variant="text"
+          size="small"
+          prepend-icon="mdi-checkbox-multiple-marked-outline"
+          :disabled="disableSelections"
+          @click="selectAll"
+      >Select All</v-btn>
+      <v-btn
+          variant="text"
+          size="small"
+          prepend-icon="mdi-checkbox-multiple-blank-outline"
+          :disabled="disableSelections"
+          @click="deselectAll"
+      >Deselect All</v-btn>
+    </div>
+  </div>
 </template>
 
 <script>
